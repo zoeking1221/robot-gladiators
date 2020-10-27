220978103bce779877ff9028f8727a33a2175625
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-10-26 22:17:30 -0400

    testing first few js commands

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+
