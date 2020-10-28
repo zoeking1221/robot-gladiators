579a5de0f82f2cdf25a8949dfd3254932e2d3fff
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-10-27 22:31:18 -0400

    added function for robot to fight all robots in enemy array

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -57,10 +54,12 @@ var fight = function() {
       else {
           fight();
       }
-      
+
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
