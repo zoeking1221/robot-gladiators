df9c3cad7355d7e8e22e8ee7ae86f2cd60e20695
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-10-31 17:38:28 -0400

    local storage high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -124,19 +124,30 @@ var startGame = function() {
             break;
         }
     }
-    // after the loop ends, player is either out of health or enemies to fight, so fun the endGame function
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
     endGame();
 };
 
 // function to end the entire game
 var endGame = function () {
-    // if player is still alive, player wins!
-    if(playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!")
+    
+    //check localstorage for highscore, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    //if player  has more money than high score, player has new high score
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     }
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");  
     }
+    // ask player if they if they want to play again?
     var playAgainConfirm = window.confirm("Would you like to play again?");
     if(playAgainConfirm) {
         // restart the game
