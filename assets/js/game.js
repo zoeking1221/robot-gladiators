c560d43ee304da8cdfd923ce57425621534649fc
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2020-10-31 14:32:59 -0400

    player name null

--- assets/js/game.js
+++ assets/js/game.js
@@ -164,8 +164,24 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function () {
+    var name = "";
+
+    // ***************************************
+    // ADD LOOP HERE WITH PROMPT AND CONDITION 
+    // ***************************************
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
+while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
